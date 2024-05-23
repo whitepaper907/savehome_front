5c8b8ef92b13a2f4d92b3dfdfc43601f99b03c69
whitepaper907 <101ghgh@naver.com>
2024-05-23 13:08:32 +0000

    fix:쿠키 개수 가져오기

--- src/components/member/KakaoLogin.vue
+++ src/components/member/KakaoLogin.vue
@@ -19,8 +19,8 @@ onMounted(() => {
   // oauth (w/spring security)
   // -- start
   getUserInfo(
-    () => {
-      // console.log(res.data)
+    (res) => {
+      console.log(res.data)
       // sessionStorage.setItem("user_id",res.data.user_id)
       // sessionStorage.setItem("user_name",res.data.user_name)
       loader.hide()
